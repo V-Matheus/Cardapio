0a705c1d01c0cb96a91236d228abe347961e2a6b
Victor Matheus <victormatheus507@gmail.com>
2024-02-02 10:27:15 -0300

    alterando as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hashtaurante from './assets/hashtaurante.webp';
 import './App.css';
 import Navegacao from './Navegacao';
 import ItemCardapio from './ItemCardapio';
 import { pratosPrincipais, sobremesas, bebidas } from './cardapio';
 const App = () => {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, setPaginaSelecionada] = useState(0);
+
   return (
     <div>
       <img src={Hashtaurante} alt="" className="capa" />
-      <Navegacao />
+      <Navegacao setPaginaSelecionada={setPaginaSelecionada} />
       <div className="menu">
-        {pratosPrincipais.map(({ nome, preco, descricao, imagem }) => (
-          <ItemCardapio
-            nome={nome}
-            preco={preco}
-            descricao={descricao}
-            imagem={imagem}
-          />
-        ))}
+        {paginasMenu[paginaSelecionada].map(
+          ({ nome, preco, descricao, imagem }) => (
+            <ItemCardapio
+              nome={nome}
+              preco={preco}
+              descricao={descricao}
+              imagem={imagem}
+            />
+          ),
+        )}
       </div>
     </div>
   );
--- src/Navegacao.jsx
+++ src/Navegacao.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-const Navegacao = () => {
+const Navegacao = ({ setPaginaSelecionada }) => {
   return (
     <div className="navegacao">
-      <input type="radio" name="opcao-pagina" id="pagina-0" defaultChecked />
+      <input
+        type="radio"
+        name="opcao-pagina"
+        id="pagina-0"
+        onClick={() => setPaginaSelecionada(0)}
+        defaultChecked
+      />
       <label htmlFor="pagina-0">Pratos Principais</label>
 
-      <input type="radio" name="opcao-pagina" id="pagina-1" />
+      <input
+        type="radio"
+        name="opcao-pagina"
+        id="pagina-1"
+        onClick={() => setPaginaSelecionada(1)}
+      />
       <label htmlFor="pagina-1">Sobremesas</label>
 
-      <input type="radio" name="opcao-pagina" id="pagina-2" />
+      <input
+        type="radio"
+        name="opcao-pagina"
+        id="pagina-2"
+        onClick={() => setPaginaSelecionada(2)}
+      />
       <label htmlFor="pagina-2">Bebidas</label>
     </div>
   );
